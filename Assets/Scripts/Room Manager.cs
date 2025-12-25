53901a6d73c03a6bd73ab98baea9786398b7104c
William <will316.work@gmail.com>
2025-12-25 13:18:49 -0800

    Added Transition Animations

--- Assets/Scripts/Room Manager.cs
+++ Assets/Scripts/Room Manager.cs
@@ -1,9 +1,33 @@
-using UnityEngine;
+using System.Collections;
 
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// 
+/// 
+/// Author: William Min
+/// Date: 12/22/25
+/// </summary>
 public class RoomManager: MonoBehaviour
 {
-    [SerializeField] private Transform _roomStorage;
-    [SerializeField] private int _currentRoomIndex;
+    #region Serialized Fields
+
+
+    [SerializeField] private Transform _roomStorage;    // 
+    [SerializeField] private int _currentRoomIndex;     // 
+
+    [Space]
+    [SerializeField] private AnimationPlayer _player;   // 
+    [SerializeField] private int _fadeInIndex;          // 
+    [SerializeField] private int _fadeOutIndex;         // 
+    [Space] public UnityEvent<int> OnSwitchToRoom;      // 
+
+
+    #endregion
+
+    #region MonoBehavior Callbacks
+
 
     protected void Awake()
     {
@@ -15,6 +39,16 @@ public class RoomManager: MonoBehaviour
         LoadRoom(_currentRoomIndex);
     }
 
+
+    #endregion
+
+    #region Public Methods
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="index"></param>
     public void LoadRoom(int index)
     {
         _roomStorage.GetChild(_currentRoomIndex).gameObject.SetActive(false);
@@ -23,4 +57,29 @@ public class RoomManager: MonoBehaviour
 
         _roomStorage.GetChild(_currentRoomIndex).gameObject.SetActive(true);
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="index"></param>
+    public void TransitionToRoom(int index)
+    {
+        StartCoroutine(_transitionToRoom(index));
+    }
+
+    private IEnumerator _transitionToRoom(int index)
+    {
+        _player.Play(_fadeInIndex);
+
+        yield return new WaitForSeconds(.15f);
+        yield return new WaitUntil(() => !_player.IsPlaying());
+
+        LoadRoom(index);
+        OnSwitchToRoom?.Invoke(_currentRoomIndex);
+
+        _player.Play(_fadeOutIndex);
+    }
+
+
+    #endregion
 }
--- Assets/Scripts/Scroll Room.cs
+++ Assets/Scripts/Scroll Room.cs
@@ -1,10 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// 
+/// 
+/// Author: William Min
+/// Date: 12/22/25
+/// </summary>
 public class ScrollRoom : MonoBehaviour
 {
-    [SerializeField] private Scrollbar _scroller;
-    [Range(-1f, 1f)] [SerializeField] private float _scrollSpeed;
+    #region Serialized Fields
+
+
+    [SerializeField] private Scrollbar _scroller;                   // 
+    [Range(-1f, 1f)] [SerializeField] private float _scrollSpeed;   // 
+
+
+    #endregion
+
+    #region MonoBehavior Callbacks
+
 
     private void Awake()
     {
@@ -19,8 +34,21 @@ public class ScrollRoom : MonoBehaviour
         _scroller.value += _scrollSpeed * Time.deltaTime;
     }
 
+
+    #endregion
+
+    #region Public Methods
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="newSpeed"></param>
     public void SetScrollSpeed(float newSpeed)
     {
         _scrollSpeed = newSpeed;
     }
+
+
+    #endregion
 }
