0da4e80b9dd19874838bf9111dc297226a81e662
William <will316.work@gmail.com>
2025-12-30 23:42:38 -0800

    Added Room Setup

---
+++ Assets/Scripts/Room Logic/Room Setup.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// 
+/// 
+/// Author: William Min
+/// Date: 12/30/25
+/// </summary>
+public class RoomSetup : MonoBehaviour
+{
+    #region Public Fields
+
+
+    /// <summary>
+    /// Events called when setting the stage.
+    /// </summary>
+    public UnityEvent DoOnStageSetup;
+
+
+    #endregion
+
+    #region Public Methods
+
+
+    /// <summary>
+    /// Sets up the stage
+    /// </summary>
+    public void SetUpStage()
+    {
+        DoOnStageSetup?.Invoke();
+    }
+
+
+    #endregion
+}
